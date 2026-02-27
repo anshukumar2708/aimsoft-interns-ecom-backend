d66439baaed6bba1c5bf671745ece5b116590017
anshu <anshukumar2708@gmail.com>
2026-02-27 16:38:06 +0530

    issues fix for multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,12 +1,10 @@
-const multer = required("multer");
+const multer = require("multer");
 
 const store = multer.memoryStorage();
 
 const upload = multer({
     storage: store,
-    limits: { filesize: 0.5 * 1024 * 1024 }
+    limits: { fileSize: 0.5 * 1024 * 1024 } // 500 KB
 });
-
-
 
 module.exports = upload;
