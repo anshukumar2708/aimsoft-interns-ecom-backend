477cc730556a1027504d03306ad4d3d095106f87
anshu <anshukumar2708@gmail.com>
2026-03-13 16:30:38 +0530

    add search implementation

--- controllers/product/productCategoryController.js
+++ controllers/product/productCategoryController.js
@@ -1,4 +1,5 @@
 const ProductCategory = require("../../models/productCategoryModel");
+const { options } = require("../../routes/productRoutes");
 const { successResponse, errorResponse } = require("../../utils/responseHandler");
 
 exports.addProductCategory = async (req, res) => {
@@ -16,8 +17,25 @@ exports.addProductCategory = async (req, res) => {
 
 exports.getProductCategory = async (req, res) => {
     try {
-        const allProductCategory = await ProductCategory.find();
+        const { search, page, limit } = req.query;
+
+        const filter = {};
+
+        const pageNumber = Number(page) || 1;
+        const limitNumber = Number(limit) || 10;
+        const skipData = (pageNumber - 1) * limitNumber;
+
+        if (search) {
+            filter.$or = [
+                { name: { $regex: search, $options: "i" } },
+                { description: { $regex: search, $options: "i" } },
+            ]
+        }
+
+        const allProductCategory = await ProductCategory.find(filter).skip(skipData).limit(limitNumber);
+
         return successResponse(res, "Category get successfully", allProductCategory);
+
     } catch (error) {
         console.error("Category get error:", error);
         return errorResponse(res, error.message);
